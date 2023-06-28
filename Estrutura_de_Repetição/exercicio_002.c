e610193243ebdb311d3274b227b236100d6937ef
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2023-06-28 17:26:10 -0300

    exercicio_2

---
+++ Estrutura_de_Repetição/exercicio_002.c
@@ -0,0 +1,28 @@
+/*. Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    char nome[50] = "";
+    char senha[50] = "";
+    
+    printf("Informe um nome de usuario: ");
+    scanf("%s", nome);
+
+    do{
+        printf("Informe uma senha: ");
+        scanf("%s", senha);
+
+        if(strcmp(nome, senha) == 0){
+
+            printf("Erro: sua senha nao pode ser igual ao nome de usuario\n");
+        }
+
+    }while(strcmp(nome, senha) == 0);
+
+    return 0;
+}
+
+/*Para comparar o conteúdo de duas strings em C, você deve usar a função strcmp() da biblioteca string.h. A função strcmp() retorna um valor inteiro que indica se as strings são iguais, maiores ou menores lexicograficamente.*/
