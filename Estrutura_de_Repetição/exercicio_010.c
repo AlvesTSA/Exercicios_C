8ee23a77026a57e54659e1cee24c884140302115
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2023-07-02 17:10:13 -0300

    exercicio_10

--- Estrutura_de_Repetição/exercicio_010.c
+++ Estrutura_de_Repetição/exercicio_010.c
@@ -31,3 +31,38 @@ int main(){
 
     return 0;
 }
+
+/*
+
+#include <stdio.h>
+
+int main() {
+    int num1, num2, i;
+
+    printf("Digite o primeiro número inteiro: ");
+    scanf("%d", &num1);
+
+    printf("Digite o segundo número inteiro: ");
+    scanf("%d", &num2);
+
+    printf("Números no intervalo entre %d e %d:\n", num1, num2);
+
+    // Verifica se num1 é maior que num2 e inverte os valores se necessário
+    if (num1 > num2) {
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+
+    // Gera e exibe os números no intervalo
+    for (i = num1 + 1; i < num2; i++) {
+        printf("%d ", i);
+    }
+
+    printf("\n");
+
+    return 0;
+}
+
+
+*/
