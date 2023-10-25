7fa5c3eec79fe91c462a98ff402cc32b5a9655f3
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2023-10-25 09:20:33 -0300

    Create exercicio_029.c

---
+++ Estrutura_de_Repetição/exercicio_029.c
@@ -0,0 +1,30 @@
+/*29. O Sr. Manoel Joaquim possui uma grande loja de artigos de R$ 1,99, com cerca de 10 caixas. Para agilizar o cálculo de quanto cada cliente deve pagar ele desenvolveu um tabela que contém o número de itens que o cliente comprou e ao lado o valor da conta. Desta forma a atendente do caixa precisa apenas contar quantos itens o cliente está levando e olhar na tabela de preços. Você foi contratado para desenvolver o programa que monta esta tabela de preços, que conterá os preços de 1 até 50 produtos, conforme o exemplo abaixo:
+
+Lojas Quase Dois - Tabela de preços 
+
+1 - R$ 1.99 
+  
+2 - R$ 3.98
+
+...
+
+50 - R$ 99.50
+*/
+
+#include <stdio.h>
+
+int main(){
+
+    float soma = 0;
+    int i = 0;
+
+    printf("Lojas Quase Dois - Tabela de precos\n\n");
+
+    for (i = 1; i <= 50; i++){
+        
+        soma += 1.99;
+        printf("%d - R$ %.2f\n",i,soma); 
+    }
+    
+    return 0;
+}
