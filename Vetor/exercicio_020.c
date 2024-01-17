a8f33c58a75bff064b6bd8a1fc53ed06fe95d3e9
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2024-01-17 14:47:52 -0300

    Update exercicio_020.c

--- Vetor/exercicio_020.c
+++ Vetor/exercicio_020.c
@@ -28,3 +28,141 @@ Relatório Final
 4 - vectra          -    9.0 -  111.1 litros - R$ 250.00
 5 - peugeout        -   14.5 -   69.0 litros - R$ 155.17
 O menor consumo é do peugeout.*/
+
+#include <stdio.h>
+
+int main(){
+
+    char carro1[20];
+    char carro2[20];
+    char carro3[20];
+    char carro4[20];
+    char carro5[20];
+    float consumo[5];
+    int i = 0;
+    float litros = 0;
+    float valor_pago = 0;
+    float menor_consumo = 0;
+
+    printf("\nComparativo de Consumo de Combustivel\n");
+
+    printf("\nVeiculo 1");
+    printf("\nNome: ");
+    scanf("%s",carro1);
+    printf("Km por litro: ");
+    scanf("%f", &consumo[0]);
+
+    printf("\nVeiculo 2");
+    printf("\nNome: ");
+    scanf("%s",carro2);
+    printf("Km por litro: ");
+    scanf("%f", &consumo[1]);
+
+    printf("\nVeiculo 3");
+    printf("\nNome: ");
+    scanf("%s",carro3);
+    printf("Km por litro: ");
+    scanf("%f", &consumo[2]);
+
+    printf("\nVeiculo 4");
+    printf("\nNome: ");
+    scanf("%s",carro4);
+    printf("Km por litro: ");
+    scanf("%f", &consumo[3]);
+
+    printf("\nVeiculo 5");
+    printf("\nNome: ");
+    scanf("%s",carro5);
+    printf("Km por litro: ");
+    scanf("%f", &consumo[4]);
+    
+
+    printf("\nRelatorio final\n");
+    menor_consumo = consumo[0];
+
+    for (i = 0; i < 5; i++){
+        
+        switch (i + 1){
+        case 1:
+
+            litros = 1000 / consumo[i];
+            valor_pago = litros * 2.25;
+
+            printf("%d - %s           -    %.1f -  %.1f litros - R$ %.2f\n", i + 1,carro1, consumo[i], litros, valor_pago);
+
+            break;
+        case 2:
+
+            litros = 1000 / consumo[i];
+            valor_pago = litros * 2.25;
+
+            printf("%d - %s           -    %.1f -  %.1f litros - R$ %.2f\n", i + 1,carro2, consumo[i], litros, valor_pago);
+
+            break;
+        case 3:
+
+            litros = 1000 / consumo[i];
+            valor_pago = litros * 2.25;
+
+            printf("%d - %s           -    %.1f -  %.1f litros - R$ %.2f\n", i + 1,carro3, consumo[i], litros, valor_pago);
+
+            break;
+        case 4:
+
+            litros = 1000 / consumo[i];
+            valor_pago = litros * 2.25;
+
+            printf("%d - %s           -    %.1f -  %.1f litros - R$ %.2f\n", i + 1,carro4, consumo[i], litros, valor_pago);
+
+            break;
+        case 5:
+
+            litros = 1000 / consumo[i];
+            valor_pago = litros * 2.25;
+
+            printf("%d - %s           -    %.1f -  %.1f litros - R$ %.2f\n", i + 1,carro5, consumo[i], litros, valor_pago);
+
+            break;
+        
+        default:
+            printf("\nValor invalido.\n");
+            break;
+        }
+
+        if (consumo[i] > menor_consumo){
+            
+            menor_consumo = consumo[i];
+        }
+        
+    }
+    for (i = 0; i < 5; i++){
+        
+        if (menor_consumo == consumo[i]){
+            break;   
+        }
+        
+    }
+    switch (i + 1){
+        case 1:
+            printf("O menor consumo e do %s.", carro1);
+            break;
+        case 2:
+            printf("O menor consumo e do %s.", carro2);
+            break;
+        case 3:
+            printf("O menor consumo e do %s.", carro3);
+            break;
+        case 4:
+            printf("O menor consumo e do %s.", carro4);
+            break;
+        case 5:
+            printf("O menor consumo e do %s.", carro5);
+            break;
+        
+        default:
+            printf("Valor invalido");
+            break;
+    }
+    
+    return 0;
+}
