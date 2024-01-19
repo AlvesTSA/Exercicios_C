12f68a906551e27c9d09310c8d6d26549e80c925
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2024-01-19 17:11:09 -0300

    Update exercicio_025.c

--- Vetor/exercicio_025.c
+++ Vetor/exercicio_025.c
@@ -4,3 +4,19 @@
 Valor       5    7    6    9     2   8    4     0     3            
 Assim, a seqüência da leitura seria 1, 5, 2, 7, 4, 9, 3, 6, 8, 0
 Faça um algoritmo que seja capaz de ler esse vetor e seguir a trajetória.*/
+
+#include <stdio.h>
+
+int main(){
+
+    int tragetoria[9] = {5,7,6,9,2,8,4,0,3};
+    int indice = 1;
+
+    for (int i = 0; i <= 9; i++){
+        
+        printf("%d ",indice);
+
+        indice = tragetoria[indice - 1];
+    }
+    return 0;
+}
