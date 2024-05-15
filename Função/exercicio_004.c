75c4a30360ff36d59fa0d3882fb9ce6662079462
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2024-05-15 17:29:15 -0300

    Update exercicio_004.c

--- Função/exercicio_004.c
+++ Função/exercicio_004.c
@@ -1,12 +1,10 @@
 /*4. Faça uma função que recebe por parâmetro o raio de uma esfera e calcula o seu volume (v = 4/3.P .R3).*/
 
 #include <stdio.h>
-#include <math.h>
-
+#define pi 3.14159265358979323846
 
 double calc_vol(double r){
 
-    double pi = M_PI;
     double v = (4.0/3.0)*pi*r*r*r;
 
     return v;
